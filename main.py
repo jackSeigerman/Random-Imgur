41e074d683de6b5e4e92d64439cdda117bfe3996
Jack Seigerman <133878087+jackSeigerman@users.noreply.github.com>
2024-06-23 11:24:25 -0500

    added tkinter

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("helloworld")
+import tkinter as tk
+from tkinter import messagebox
+
+def on_button_click():
+    messagebox.showinfo("Info", "Button clicked!")
+
+root = tk.Tk()
+root.title("Tkinter Example")
+
+button = tk.Button(root, text="Click Me", command=on_button_click)
+button.pack(pady=20)
+
+root.mainloop()
