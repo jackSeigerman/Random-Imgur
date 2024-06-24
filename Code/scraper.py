8550fafdb4c4992168ff9de5361beb694b3d86e6
abloom22 <aryehb113@gmail.com>
2024-06-24 13:34:20 -0400

    fixed getId
    now work

--- Code/scraper.py
+++ Code/scraper.py
@@ -2,16 +2,16 @@ import random
 
 
 def getIdentifier():
-    str = ''
-    while str.len() < 5:
-        i = random.randint(0, 63)
+    id = ''
+    while len(id) < 5:
+        i = random.randint(0, 61)
         if i >= 0 and i <=9:
-            str += i
-        elif i>=10 and i <= 36:
-            str += chr(i+55)
+            id += str(i)
+        elif i>=10 and i <= 35:
+            id += chr(i+55)
         else:
-            str += chr(i+61)
-    print(str)
+            id += chr(i+61)
+    print(id)
 
 
 getIdentifier()
