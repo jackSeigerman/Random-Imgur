6cd3d5142dd65b2dde98dd3de141c7d25e65ffd4
Jack Seigerman <133878087+jackSeigerman@users.noreply.github.com>
2024-06-26 21:42:19 -0500

    image resizing works

--- Gui.java
+++ Gui.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class Gui
 {
     // initialize GUI Elements
-    private final JLabel imageLabel = new JLabel();
+    private final ImagePanel imagePanel = new ImagePanel();
     private final JButton randomImageButton = new JButton("Generate Image");
 
     public static void main(String[] args)
@@ -17,7 +17,6 @@ public class Gui
     // Method to create and display the GUI
     private void createAndShowGUI()
     {
-
         // gui create
         JFrame gui = new JFrame("Random Image Generator");
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,22 +24,19 @@ public class Gui
         gui.setSize(500, 400);
         gui.setVisible(true);
 
-
-        //image location
-        gui.add(imageLabel, BorderLayout.CENTER);
-
+        // image location
+        gui.add(imagePanel, BorderLayout.CENTER);
 
         // listens for click on button
-        gui.add(randomImageButton, BorderLayout.NORTH);
+        gui.add(randomImageButton, BorderLayout.SOUTH);
         randomImageButton.addActionListener(_ -> generateAndDisplayImage());
-
     }
 
     // Method to generate a random string and display the image
     private void generateAndDisplayImage()
     {
         String text = RandomImageGenerator.generateRandomString();
-        //upon success of fetching image
+        // upon success of fetching image
         try
         {
             BufferedImage image = RandomImageGenerator.fetchRandomImage(text);
@@ -54,7 +50,7 @@ public class Gui
                 // If the image is valid, display it
                 if (image != null)
                 {
-                    imageLabel.setIcon(new ImageIcon(image));
+                    imagePanel.setImage(image);
                 }
             }
         }
@@ -62,6 +58,55 @@ public class Gui
         catch (IOException e)
         {
             generateAndDisplayImage();
+        }
+    }
+
+    private static class ImagePanel extends JPanel
+    {
+        private BufferedImage image;
+
+        @Override
+        protected void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);
+            if (image != null)
+            {
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+                g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                int panelWidth = getWidth();
+                int panelHeight = getHeight();
+                int imageWidth = image.getWidth();
+                int imageHeight = image.getHeight();
+
+                double panelAspect = (double) panelWidth / panelHeight;
+                double imageAspect = (double) imageWidth / imageHeight;
+
+                int drawWidth, drawHeight;
+                if (panelAspect > imageAspect)
+                {
+                    drawHeight = panelHeight;
+                    drawWidth = (int) (panelHeight * imageAspect);
+                }
+                else
+                {
+                    drawWidth = panelWidth;
+                    drawHeight = (int) (panelWidth / imageAspect);
+                }
+
+                int x = (panelWidth - drawWidth) / 2;
+                int y = (panelHeight - drawHeight) / 2;
+
+                g2d.drawImage(image, x, y, drawWidth, drawHeight, this);
+            }
+        }
+
+        public void setImage(BufferedImage image)
+        {
+            this.image = image;
+            repaint();
         }
     }
 }
