4625e8eb22c858564f1246e402da8cea0e0a5200
abloom22 <aryehb113@gmail.com>
2024-06-27 16:44:16 -0400

    gallery work n stuff

--- gallery.java
+++ gallery.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -5,9 +7,20 @@ public class gallery
 {
     private static Queue<String> galleryIds = new LinkedList<String>();
 
+    public imageResizer topLeft = new imageResizer();
+    public imageResizer topCenter = new imageResizer();
+    public imageResizer topRight = new imageResizer();
+    public imageResizer midLeft = new imageResizer();
+    public imageResizer midCenter = new imageResizer();
+    public imageResizer midRight = new imageResizer();
+    public imageResizer botLeft = new imageResizer();
+    public imageResizer botCenter = new imageResizer();
+    public imageResizer botRight = new imageResizer();
+
+
     //saves the last 10 image ids to a queue
     public static void enqueu(String id){
-        if(galleryIds.size() > 9){
+        if(galleryIds.size() > 8){
             galleryIds.remove();
         }
         galleryIds.add(id);
@@ -18,5 +31,22 @@ public class gallery
     {
         // Add functionality for the gallery button here
         System.out.println("Gallery Button Clicked");
+        new gallery().createAndShowGallery();
+
+    }
+
+    private void createAndShowGallery(){
+        System.out.println("Creating Gallery");
+        JFrame gal = new JFrame("Gallery");
+        gal.setDefaultCloseOperation(gal.EXIT_ON_CLOSE);
+        gal.setLayout(new BorderLayout());
+        gal.setSize(500, 400);
+
+        gal.setVisible(true);
+
+        gal.add(topLeft, BorderLayout.NORTH);
+        gal.add(topCenter, BorderLayout.CENTER);
+        gal.add(topRight, BorderLayout.EAST);
+
     }
 }
