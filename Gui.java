50c22b470eda6db49d677253f4f89c8e40a9d4b6
Jack Seigerman <133878087+jackSeigerman@users.noreply.github.com>
2024-06-26 21:46:45 -0500

    seperated files

--- Gui.java
+++ Gui.java
@@ -28,7 +28,7 @@ public class Gui
         gui.add(imagePanel, BorderLayout.CENTER);
 
         // listens for click on button
-        gui.add(randomImageButton, BorderLayout.SOUTH);
+        gui.add(randomImageButton, BorderLayout.NORTH);
         randomImageButton.addActionListener(_ -> generateAndDisplayImage());
     }
 
@@ -58,55 +58,6 @@ public class Gui
         catch (IOException e)
         {
             generateAndDisplayImage();
-        }
-    }
-
-    private static class ImagePanel extends JPanel
-    {
-        private BufferedImage image;
-
-        @Override
-        protected void paintComponent(Graphics g)
-        {
-            super.paintComponent(g);
-            if (image != null)
-            {
-                Graphics2D g2d = (Graphics2D) g;
-                g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-                g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-                int panelWidth = getWidth();
-                int panelHeight = getHeight();
-                int imageWidth = image.getWidth();
-                int imageHeight = image.getHeight();
-
-                double panelAspect = (double) panelWidth / panelHeight;
-                double imageAspect = (double) imageWidth / imageHeight;
-
-                int drawWidth, drawHeight;
-                if (panelAspect > imageAspect)
-                {
-                    drawHeight = panelHeight;
-                    drawWidth = (int) (panelHeight * imageAspect);
-                }
-                else
-                {
-                    drawWidth = panelWidth;
-                    drawHeight = (int) (panelWidth / imageAspect);
-                }
-
-                int x = (panelWidth - drawWidth) / 2;
-                int y = (panelHeight - drawHeight) / 2;
-
-                g2d.drawImage(image, x, y, drawWidth, drawHeight, this);
-            }
-        }
-
-        public void setImage(BufferedImage image)
-        {
-            this.image = image;
-            repaint();
         }
     }
 }
---
+++ ImagePanel.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+// Custom panel for displaying an image
+public class ImagePanel extends JPanel
+{
+    // The image to be displayed
+    private BufferedImage image;
+
+    // Override the paintComponent method to draw the image
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g); // Call the superclass's paintComponent method
+        if (image != null)
+        {
+            // Use Graphics2D for better rendering control
+            Graphics2D g2d = (Graphics2D) g;
+
+            // Set rendering hints for better quality
+            g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            // Get the dimensions of the panel and the image
+            int panelWidth = getWidth();
+            int panelHeight = getHeight();
+            int imageWidth = image.getWidth();
+            int imageHeight = image.getHeight();
+
+            // Calculate the aspect ratios
+            double panelAspect = (double) panelWidth / panelHeight;
+            double imageAspect = (double) imageWidth / imageHeight;
+
+            // Calculate the dimensions for the scaled image
+            int drawWidth, drawHeight;
+            if (panelAspect > imageAspect)
+            {
+                drawHeight = panelHeight;
+                drawWidth = (int) (panelHeight * imageAspect);
+            }
+            else
+            {
+                drawWidth = panelWidth;
+                drawHeight = (int) (panelWidth / imageAspect);
+            }
+
+            // Calculate the position to center the image
+            int x = (panelWidth - drawWidth) / 2;
+            int y = (panelHeight - drawHeight) / 2;
+
+            // Draw the image
+            g2d.drawImage(image, x, y, drawWidth, drawHeight, this);
+        }
+    }
+
+    // Method to set the image and repaint the panel
+    public void setImage(BufferedImage image)
+    {
+        this.image = image;
+        repaint(); // Trigger a repaint to display the new image
+    }
+}
